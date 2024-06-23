2aabd280b23d6ede11e3c55088b2d93e829719f6
Manvi Garg <91490442+Manvi008@users.noreply.github.com>
2024-06-23 22:52:04 +0530

    Create increment.cpp

---
+++ increment.cpp
@@ -0,0 +1,26 @@
+/*Increment 
+  post increment i++(increase i value after once used )
+  ore increment ++i (incresed the value of i before use)*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    int i=5;
+    ++i;
+    cout<<i<<endl;
+    i++;
+    cout<<i<<endl;
+}
+/*output 6 
+       7*/
+
+
+/* #include<iostream>
+using namespace std;
+int main(){
+    int i=5;
+   cout<< i++;
+   
+}
+output=5
+*/ 
