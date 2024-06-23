87e3d383d7d6d04925aa06fef7973e663131c1da
Manvi Garg <91490442+Manvi008@users.noreply.github.com>
2024-06-23 19:43:25 +0530

    Update BitwiseOperator_Implementation.cpp

--- BitwiseOperator_Implementation.cpp
+++ BitwiseOperator_Implementation.cpp
@@ -9,3 +9,9 @@ cout<<"the ^ operation on a and b is "<<a^b;
 cout<<"the ~ operation on a  is "<<~a;
 return 0;
 }
+/*
+the & operation on a and b is 4
+the | operation on a and b is 6
+the ^ operation on a and b is 2
+the ~ operation on a  is -5
+not; we use 2's compliment when we ar needed to print any negative number */
