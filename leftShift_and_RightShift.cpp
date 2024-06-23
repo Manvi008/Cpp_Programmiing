bef5e202feeb845675ed17760d23cc95bd2cf3e4
Manvi Garg <91490442+Manvi008@users.noreply.github.com>
2024-06-23 19:57:34 +0530

    Create leftShift_and_RightShift.cpp

---
+++ leftShift_and_RightShift.cpp
@@ -0,0 +1,9 @@
+/*
+  Lets talk about left shift and right shift now   for example if we want to  do the left shift it will be like 5<<1 , the expansion of 5 is 94 bytes,32 bits)00000--------101 
+  ,so its left shift means shifting one bit by left which means 0000000000.....1010.
+  example; 3<<2,which  means  left shift 3 by 2 0000.....011 after 3<<2 will be 00000.....1100
+
+  Lets talk about right shift now 
+example 15>>1 o/p=1
+NOTE :padding with always 0 in positive numbers
+*/
