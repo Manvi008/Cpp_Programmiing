a25cbf304557a492d0b46e9ea746bb0be89dba67
Manvi Garg <91490442+Manvi008@users.noreply.github.com>
2024-06-23 19:36:12 +0530

    Create BitwiseOperator_Implementation.cpp

---
+++ BitwiseOperator_Implementation.cpp
@@ -0,0 +1,14 @@
+#inlcude<iostream>
+using namespace std
+int main(){
+  int a=4;
+int b=6;
+cout<<"the & operation on a and b is "<<a&b;
+cout<<"the | operation on a and b is "<<a|b;
+cout<<"the ^ operation on a and b is "<<a^b;
+cout<<"the ~ operation on a  is "<<~a;
+return 0;
+}
+
+
+  
