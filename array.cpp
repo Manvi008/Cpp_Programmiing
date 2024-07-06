28d1148b05ed1012dd500d8c4ec645ef43a5e103
Manvi Garg <91490442+Manvi008@users.noreply.github.com>
2024-07-07 00:20:09 +0530

    Update array.cpp

--- array.cpp
+++ array.cpp
@@ -10,3 +10,16 @@ int main(){
     
     return 0;
 }
+/*to tell the size of array*/
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[ ]={1,2,3,4,5};
+    cout<<"size of an array element 1 is"<<sizeof(arr[0]);
+    cout<<"size of the array is "<<sizeof(arr);
+    int n;
+    cout<<"number of elements in the array is"<<sizeof(arr)/sizeof(arr[0]);
+
+    
+    return 0;
+}
