788974223f9ae682546fa4af8fa437f47fad50fe
Manvi Garg <91490442+Manvi008@users.noreply.github.com>
2024-07-07 00:15:14 +0530

    Update array.cpp

--- array.cpp
+++ array.cpp
@@ -1,1 +1,12 @@
 /* array*/
+/*traversing an array*/
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[5]={1,2,3,4,5};
+    for(int i=0;i<5;i++){
+        cout<<arr[i];
+    }
+    
+    return 0;
+}
